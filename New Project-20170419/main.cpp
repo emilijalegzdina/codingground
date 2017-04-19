8ec86ab2f3f27e943a4c7c642215714061907f19
emilijalegzdina <emilija.legzdina@gmail.com>
2017-04-19 13:16:23 +0300

    Created by Coding Ground Client

---
+++ New Project-20170419/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+   int x;
+
+ 
+ cout <<  "Ievadi savu videjo atzimi " << endl;
+ cin >> x;
+ 
+ if(x == 10){
+     cout << " Izcili " <<  endl;
+     
+ } else if( x == 9){
+ 
+   cout << " teicami "  <<  endl;
+   
+ } else if ( x == 8){
+   
+   cout << " loti labi " <<  endl;
+   
+ } else if ( x == 7){
+     
+      cout << " labi " <<  endl;
+      
+ } else if ( x == 6){
+     
+     cout << " gandriz labi " <<  endl;
+ } else if ( x == 5){
+       cout << " viduveji " <<  endl;
+ } else if ( x == 4){
+     cout << " gandriz viduveji " <<  endl; 
+ } else if ( x == 3){
+     cout << " vaji  " <<  endl;
+ } else if ( x == 2){
+       cout << " loti vaji " <<  endl;
+ } else if ( x == 1){
+     cout << " loti loti vaji " <<  endl;
+   return 0;
+}
+}
